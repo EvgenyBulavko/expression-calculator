bcb83f8c03c07c39784989dcbf928cc55cfd1eeb
Bulavko Evgeniy <z-bulavko@mail.ru>
2020-03-20 23:58:53 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -5,7 +5,38 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+
+    let proverkamatr;
+    proverkamatr = expr.split("");
+    ProverBrackets(proverkamatr);
+    ProverZero(proverkamatr);
+    let s;
+    s = expr.replace(/\s/g, "");
+    let Resh = new Function(`return ${s}`);
+
+    return Resh();
 }
+
+function ProverBrackets(proverkamatr) {
+    let kol = 0;
+
+    for (let value = 0; value < proverkamatr.length; value++) {
+        if (proverkamatr[value] === ")") kol--;
+        else if (proverkamatr[value] === "(") kol++;
+        if (kol < 0) break;
+    }
+
+    if (kol > 0 || kol < 0) throw new Error("ExpressionError: Brackets must be paired");
+}
+
+function ProverZero(proverkamatr) {
+    for (let i = 0; i < proverkamatr.length; i++) {
+        if (proverkamatr[i] === "/" && proverkamatr[i + 2] === "0") {
+            throw new Error("TypeError: Division by zero.");
+        }
+    }
+}
+
 
 module.exports = {
     expressionCalculator
